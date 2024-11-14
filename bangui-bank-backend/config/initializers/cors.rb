1fbd1f51f2d987d58c955b2feacc4793be518040
Moise Ngoumape <mngoumape@brotherhoodmutual.com>
2024-11-14 16:19:35 -0500

    adding backend

--- bangui-bank-backend/config/initializers/cors.rb
+++ bangui-bank-backend/config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:8080',
-            'https://bank-bangui-ohgvwa.fly.dev'
+            'https://bangui-bank-frontend.fly.dev'
             'http://142.93.6.64',
             'http://142.93.6.64:3000'
 
