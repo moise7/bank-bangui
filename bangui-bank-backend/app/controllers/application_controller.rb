19993430972033285cb64cfe1d19c5392e62bc0b
Moise Ngoumape <mngoumape@brotherhoodmutual.com>
2024-12-05 14:41:33 -0500

    fixing everything so far

--- bangui-bank-backend/app/controllers/application_controller.rb
+++ bangui-bank-backend/app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   private
@@ -6,16 +6,18 @@ class ApplicationController < ActionController::API
   def authenticate_user!
     return if request.headers['Authorization'].blank?
 
+    token = request.headers['Authorization'].split(' ').last
+
     begin
-      token = request.headers['Authorization'].split(' ').last
       decoded_token = JwtService.decode(token)
 
-      if decoded_token
+      if decoded_token && decoded_token['user_id']
         @current_user = User.find_by(id: decoded_token['user_id'])
       end
     rescue JWT::DecodeError => e
       Rails.logger.error "JWT decode error: #{e.message}"
-      nil
+    rescue JWT::ExpiredSignature
+      Rails.logger.error "JWT expired"
     end
 
     render json: { error: 'Non autorisé' }, status: :unauthorized unless @current_user
--- bangui-bank-backend/app/javascript/application.js
+++ bangui-bank-backend/app/javascript/application.js
@@ -1,1 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "./controllers" // For Turbo and Stimulus (if using)
+import "./stylesheets/application.css" // Import CSS if necessary
